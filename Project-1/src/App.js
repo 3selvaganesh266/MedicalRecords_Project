47d67cdff51d8ac0ac4654e42ddf1f6008421136
selvaganesh <selvaganesh12p@gmail.com>
2023-12-13 21:23:28 +0530

    Project's basics

---
+++ Project-1/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import LoginSignup from './components/LoginSignup';
+
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
